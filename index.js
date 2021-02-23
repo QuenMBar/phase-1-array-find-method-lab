3ff1789003b9b94291b0fdae603a8b6959cd609a
QuenMBar <Qbeast360@gmail.com>
2021-02-23 15:41:20 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function superbowlWin(records) {
+    let record = records.find(rec => rec.result === "W");
+    if (record === undefined) {
+        return undefined;
+    } else {
+        return record.year;
+    }
+
+}
